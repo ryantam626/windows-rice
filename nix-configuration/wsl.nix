5e5491bda4643f9e44ba5ffff4f029dcf2c89ba1
Ryan Tam <ryantam626@gmail.com>
2024-12-07 16:31:06 +0000

    kernel settings

--- nix-configuration/wsl.nix
+++ nix-configuration/wsl.nix
@@ -81,4 +81,10 @@
       options = "--delete-older-than 7d";
     };
   };
+
+  boot.kernel.sysctl = {
+    "fs.inotify.max_user_watches"   = 1048576;   # default:  8192
+    "fs.inotify.max_user_instances" =    1024;   # default:   128
+    "fs.inotify.max_queued_events"  =   32768;   # default: 16384
+  };
 }
