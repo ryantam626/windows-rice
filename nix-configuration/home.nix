8d038ca9c79e186e2eb508373eea69756236544b
Ryan Tam <ryantam626@gmail.com>
2024-12-07 16:16:05 +0000

    setting

--- nix-configuration/home.nix
+++ nix-configuration/home.nix
@@ -103,5 +103,77 @@ in {
         plugins = [ "git" "zoxide" ];
       };
     };
+
+    tmux = {
+      aggressiveResize = true;
+      baseIndex = 1;
+      clock24 = true;
+      enable = true;
+      terminal = "screen-256color";
+      package = pkgs.unstable.tmux;
+      keyMode = "vi";
+      prefix = "C-a";
+      historyLimit = 100001;
+      plugins = with pkgs; [
+        tmuxPlugins.sensible
+        tmuxPlugins.nord
+        {
+          plugin = tmuxPlugins.yank;
+          extraConfig = ''
+            set -g @yank_selection_mouse 'clipboard'
+            set -g @override_copy_command 'copy'
+          '';
+        }
+        {
+          plugin = tmuxPlugins.tmux-thumbs;
+          extraConfig = ''
+            set -g @thumbs-key v
+            set -g @thumbs-position off_left
+            set -g @thumbs-command 'echo -n {} | /mnt/c/Windows/System32/clip.exe'
+          '';
+        }
+      ];
+      extraConfig = ''
+        # Split panes using Prefix+- and Prefix+_ instead of Prefix+" and Prefix+%
+        bind-key _ split-window -h -c '#{pane_current_path}'
+        bind-key - split-window -v -c '#{pane_current_path}'
+        unbind-key '"'
+        unbind-key '%'
+
+        # Pane nav
+        is_vim="ps -o state= -o comm= -t '#{pane_tty}' | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?)(diff)?$'"
+        bind-key -n 'M-h' if-shell "$is_vim" 'send-keys C-h'  'select-pane -L'
+        bind-key -n 'M-j' if-shell "$is_vim" 'send-keys C-j'  'select-pane -D'
+        bind-key -n 'M-k' if-shell "$is_vim" 'send-keys C-k'  'select-pane -U'
+        bind-key -n 'M-l' if-shell "$is_vim" 'send-keys C-l'  'select-pane -R'
+
+        bind-key -T copy-mode-vi 'M-h' select-pane -L
+        bind-key -T copy-mode-vi 'M-j' select-pane -D
+        bind-key -T copy-mode-vi 'M-k' select-pane -U
+        bind-key -T copy-mode-vi 'M-l' select-pane -R
+
+        # Create window with Ctrl+T instead of Prefix+c
+        bind -n C-t new-window
+        unbind-key 'c'
+
+        # Alt+[ for copy mode
+        bind -n M-[ copy-mode
+
+        # Move to new window with Alt+Shift+H/L
+        bind-key -n M-H previous-window
+        bind-key -n M-L next-window
+
+        # Setup 'v' to begin selection as in Vim
+        bind-key -T copy-mode-vi v send -X begin-selection
+
+        # make colors inside tmux look the same as outside of tmux
+        # see https://github.com/tmux/tmux/issues/696
+        # see https://stackoverflow.com/a/41786092
+        set-option -ga terminal-overrides ",xterm-256color:Tc"
+        # bg color of active pane, https://www.color-hex.com/color-palette/1029048, darkest
+        setw -g window-active-style 'bg=#292e39'
+
+       '';
+    };
   };
 }
