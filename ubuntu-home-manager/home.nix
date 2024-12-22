029b8f4dcc000c65773dc753a6bbcb037e2827f0
Ryan Tam <ryantam626@gmail.com>
2024-12-22 11:08:02 +0000

    feat: Alter PATH for poetry

--- ubuntu-home-manager/home.nix
+++ ubuntu-home-manager/home.nix
@@ -94,6 +94,10 @@ in {
         [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
         eval "$(pyenv init -)"
         # }}
+
+        # poetry {{
+        export PATH="$HOME/.local/bin:$PATH"
+        # }}
       '';
       shellAliases = {
         pbcopy = "/mnt/c/Windows/System32/clip.exe";
